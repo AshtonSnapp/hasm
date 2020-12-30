11b26c8589af7b55b0584ec7cb5d087f1c5456de
Ashton Snapp <snappgamez@outlook.com>
2020-12-30 13:55:38 -0600

    main() & clap now do basic cli
    - basic cli implemented, you can do help and version but not much else
    - main() now invokes lex::lex() but nothing happens after that point.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 extern crate clap;
 use clap::*;
 use std::path::Path;
+use std::process::exit;
 
 mod lex;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = clap::App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand_name() {
+        None => {
+            eprintln!("ERR: No sub-command! What am I supposed do do with NOTHING?");
+            exit(1);
+        }
+        Some("assemble") => {
+            let asm_matches = matches.subcommand_matches("assemble").unwrap();
+            let mut v = false;
+            if asm_matches.is_present("verbose") {
+                v = true;
+            }
+
+            let infile = Path::new(asm_matches.value_of("INPUT").unwrap());
+            let spt = usize::from_str_radix(asm_matches.value_of("spt").unwrap(), 10).unwrap();
+            let tokens = lex::lex(infile, v, spt);
+        }
+        _ => {}
+    }
 }
